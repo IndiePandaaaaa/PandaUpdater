874b5cdf786bba2d1f2b5d6a395f920e78f7a9eb
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-12-02 16:33:38 +0100

    Update PandaUpdater.bat: added closing of apps which interrupt the update process

--- PandaUpdater.bat
+++ PandaUpdater.bat
@@ -2,6 +2,9 @@
 rem encoding: utf-8
 rem created by IndiePandaaaaa
 
+echo "PANDA UPDATER"
+echo "--- searching for updates ---"
+
 rem exclude packages which do not work
 winget pin add Apple.AppleMobileDeviceSupport >nul 2>&1
 winget pin add Apple.iTunes >nul 2>&1
@@ -11,11 +14,14 @@ winget pin add Microsoft.VisualStudio.2022.Community >nul 2>&1
 winget pin add Ubisoft.Connect >nul 2>&1
 winget pin add Waterfox.Waterfox >nul 2>&1
 
-rem TODO: check if ferdium is running and close it, because it blocks updates for OBS, Ferdium, Python
-rem taskkill /im "test.exe" /f >nul 2>&1
+echo "--- closing apps for updates ---"
+taskkill /im "Ferdium.exe" /f >nul 2>&1
+taskkill /im "obs32.exe" /f >nul 2>&1
+taskkill /im "obs64.exe" /f >nul 2>&1
+taskkill /im "python.exe" /f >nul 2>&1
 
-echo "--- starting update ---"
+echo "--- starting updates ---"
 winget upgrade --all --include-unknown --accept-source-agreements --accept-package-agreements --silent
 
-echo "--- update finished ---"
+echo "UPDATES FINISHED"
 pause
